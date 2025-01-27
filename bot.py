44bb2ddeb3a9c5a39b7719517a3f29008b84202f
Arsalan Radnaev <radnaevarsalan0@gmail.com>
2025-01-28 07:59:34 +0800

    bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher
 # Замените "YOUR_BOT_TOKEN" на токен, который вы получили от BotFather
-API_TOKEN = '7540685526:AAGH7PK5BI01yl7OH4hV9Sf3cWsLSQzjxnU'
+API_TOKEN = '7540685526:AAGM190RgMsgNWELvWYmplcl8jbmFfsiJnQ'
 
 # Объект бота
 bot = Bot(token=API_TOKEN)
